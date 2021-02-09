50463df0171788e742cabe872baa2030a85efea3
MrAPauls <55466660+MrAPauls@users.noreply.github.com>
2021-02-09 13:46:00 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+## Functions Assignment
+#Name:
+#Date:
+
+
+#Task 1.
+#Create a function that takes in two numbers
+#and determines if both are greater than 10. Return True or False
+#in: two numbers
+#do: determine if both numbers are greater than 10
+#out: True or False
+
+def two_ten(num1, num2):
+    pass
+
+
+#Task 2.
+#A grocery list has 10 spaces for items.
+#Given a list, determine if it is full or not. Empty spaces are ' '.
+#in: list
+#do: determine if it contains any empty spaces
+#out: True or Fals
+
+def list_full(lst):
+    pass
+
+#Task 3.
+#To add to the grocery list, the user gives
+#an item and the position on the list to add it.
+#if there is already something in that position, overwrite it.
+#in: grocery list (list), item to add (string), position to add (int)
+#do: changed list
+#out: None
+
+def add_groc(lst, item, pos):
+    pass
+
+
+#Task 4.
+#Ask the user the name of the store they will shop at.
+#Change the value of the GLOBAL variable storeName
+#in: None
+#do: ask for user input, get store name, change storeName variable
+#out: None
+
+storeName = '' #global variable
+
+def get_Store():
+    pass
+
+
+
+
+
+
