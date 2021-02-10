24ee570fcb549100819d2fff14203a59696af982
Andrew Pauls <55466660+MrAPauls@users.noreply.github.com>
2021-02-10 17:10:49 -0400

    started working on solution

--- solution.py
+++ solution.py
@@ -11,32 +11,18 @@
 #out: True or False
 
 def two_ten(num1, num2):
-    if num1 > 10 and num2 > 10: return True
-    else: return False
+    pass
 
-print("Testing two_ten")
-print(two_ten(3, 4) == False)
-print(two_ten(11, 12) == True)
-print(two_ten(5, 11) == False)
-print(two_ten(111, 1) == False)
 
 #Task 2.
 #A grocery list has 10 spaces for items.
 #Given a list, determine if it is full or not. Empty spaces are ' '.
 #in: list
-#do: determine if it it is full
+#do: determine if it contains any empty spaces
 #out: True or Fals
 
-groc = ['banana', 'lettuce', ' ', ' ', ' ']
-
 def list_full(lst):
-    if lst.count(' ') == 0:
-        return True
-    else: return False
-
-print('Testing list_full')
-print(list_full(["a", "b", "c"]) == True)
-print(list_full([" ", 'b', 'c']) == False)
+    pass
 
 #Task 3.
 #To add to the grocery list, the user gives
@@ -50,7 +36,6 @@ def add_groc(lst, item, pos):
     pass
 
 
-
 #Task 4.
 #Ask the user the name of the store they will shop at.
 #Change the value of the GLOBAL variable storeName
@@ -62,9 +47,6 @@ storeName = '' #global variable
 
 def get_Store():
     pass
-
-get_Store()  #give it  "Superstore"
-print(storeName == "Superstore")
 
 
 
